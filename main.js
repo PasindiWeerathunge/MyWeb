8c608db1041e6cf54b5027c58e5de4ce13dcda3c
Pasindi Weerathunge <dmppw96@gmail.com>
2020-11-14 20:35:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
 
-            let fname, lname, age, email, tel_no, courses, year;
+            // let fname, lname, age, email, tel_no, courses, year;
             
-            fname = document.getElementById("f_name").value;
-            lname = document.getElementById("L_name").value;
-            age = document.getElementById("age").value;
-			email = document.getElementById("email").value;
-			tel_no = document.getElementById("tel").value;
-			courses = document.getElementById("courses").value;
-			year = document.getElementById("A_period").value;
+            // fname = document.getElementById("f_name").value;
+            // lname = document.getElementById("L_name").value;
+            // age = document.getElementById("age").value;
+			// email = document.getElementById("email").value;
+			// tel_no = document.getElementById("tel").value;
+			// courses = document.getElementById("courses").value;
+			// year = document.getElementById("A_period").value;
 			
-			document.getElementById("dislpayVal1").innerHTML = fname;
-			document.getElementById("dislpayVal2").innerHTML = lname;
+			// document.getElementById("dislpayVal1").innerHTML = fname;
+			// document.getElementById("dislpayVal2").innerHTML = lname;
 			
-			document.getElementById("dislpayyear").innerHTML = year;
-			document.getElementById("dislpayCourses").innerHTML = courses;
+			// document.getElementById("dislpayyear").innerHTML = year;
+			// document.getElementById("dislpayCourses").innerHTML = courses;
+
+			function DispalyValue(){
+
+				let fname, lname;
+						
+						fname = document.getElementById("f_name").value;
+						lname = document.getElementById("L_name").value;
+					   
+						
+						document.getElementById("val1").innerHTML = "First and Last name is "+fname +" " + lname;
+						//document.getElementById("val2").innerHTML = lname;
+			
+			
+			}
 			
 
             
