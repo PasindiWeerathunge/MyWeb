f30076573d3f9d577e863f0e044945070a51007d
Pasindi Weerathunge <dmppw96@gmail.com>
2020-11-14 11:19:31 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+
+            let fname, lname, age, email, tel_no, courses, year;
+            
+            fname = document.getElementById("f_name").value;
+            lname = document.getElementById("L_name").value;
+            age = document.getElementById("age").value;
+			email = document.getElementById("email").value;
+			tel_no = document.getElementById("tel").value;
+			courses = document.getElementById("courses").value;
+			year = document.getElementById("A_period").value;
+			
+			document.getElementById("dislpayVal1").innerHTML = fname;
+			document.getElementById("dislpayVal2").innerHTML = lname;
+			
+			document.getElementById("dislpayyear").innerHTML = year;
+			document.getElementById("dislpayCourses").innerHTML = courses;
+			
+
+            
+    
